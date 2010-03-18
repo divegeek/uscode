6fc21e3d284fc5666194ccbb3ad5dde187d40f45
Shawn Willden <shawn@willden.org>
2010-03-18 10:50:07 -0600

    Modified update script to skip commit if nothing changed.

--- src/update_uscode.sh
+++ src/update_uscode.sh
@@ -4,7 +4,11 @@ pushd ~/sources/uscode
 git pull origin master
 rm -rf code
 python /home/shawn/sources/uscode/src/retrieve_code.py .
-git commit -m "`date`" -a
-git tag -f -a -m "Daily tag" `date +"%F"`
-git push --tags origin master
+git add code
+git status > /dev/null
+if [ $? -eq 0 ]; then
+    git commit -m "`date`" -a
+    git tag -f -a -m "Daily tag" `date +"%F"`
+    git push --tags origin master
+fi
 popd
